fa2b1cd66f7fc00ce813492788b273df297102a0
Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
2023-06-12 10:02:16 -0700

    Update vite.config.ts

--- profile/vite.config.ts
+++ profile/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/zfreiter.github.io/',
+  base: '/zfreiter.github.io/profile/',
 });
